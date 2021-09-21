40c3aecca31e00eb302c2a4fa06082a329cddb9f
Rachana <giliyalrachana@gmail.com>
2021-09-21 21:57:33 +0530

    Arrays added
    Added new classes

---
+++ ArraysAndCollections/ArraysAndCollections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ArraysAndCollections
+{
+    // This program Demo. overview of an array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = { 10, 20, 30, 40, 50 };
+            numsArray[0] = 11;
+            Console.WriteLine($"numsArray[0]={numsArray[0]}");
+
+            foreach (var num in numsArray)
+            {
+                Console.Write($"{num}\t");
+            }
+            Console.WriteLine();
+
+            string[] strArray = { "string1", "string2", "string3","string4","string5" };
+            strArray[0] = "New String";
+            Console.WriteLine($"strArray[0]={strArray[0]}");
+
+            foreach (var str in strArray)
+            {
+                Console.Write($"{str}\t");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
